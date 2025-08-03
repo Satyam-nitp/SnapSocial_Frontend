b1d1582e9964bf0612b113d354645846fd965bcc
Satyam-nitp <satyamsingh2004alld@gmail.com>
2025-08-03 18:43:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='font-bold text-center text-3xl h-screen flex justify-center items-center bg-slate-700 text-white'>
+      Starter Pack is ready
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
